5a8ba1d4ae209bd7fb468ec2ddcc26f0bfd2ea2a
tamayama <h.tamayama.sys24@morijyobi.ac.jp>
2026-01-21 12:11:19 +0900

    ヘッターとフッターの作成

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from . import views
+
 app_name = "main"
-urlpatterns = []
+urlpatterns = [
+    path("recruitment/", views.recruitment, name="recruitment"),
+    path("business_activities/", views.business_activities, name="business_activities"),
+    path("company_information/", views.company_information, name="company_information"),
+]
