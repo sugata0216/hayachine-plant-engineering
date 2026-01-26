f64cfea8141788a5e5c34de6da3a25ccf20bdea4
masaya <m.shimotani.sys24@morijyobi.ac.jp>
2026-01-26 10:20:57 +0900

    お問い合わせページの作成

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,5 @@
 from django.urls import path
+from . import views
+
 app_name = "main"
-urlpatterns = []
+urlpatterns = [path("inquiry/", views.inquiry_view)]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def inquiry_view(request):
+    return render(request, "main/inquiry.html")
