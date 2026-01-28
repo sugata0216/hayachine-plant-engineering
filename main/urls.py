a58ab3f5c2074eeee5337365c5f89dec680ac6fe
sugata <k.sugata.sys24@morijyobi.ac.jp>
2026-01-28 23:09:59 +0900

    ヘッダー・フッターの追加、細かいところの修正

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+app_name = "main"
 urlpatterns = [
     path("", views.index, name="index"),
     path("news/", views.news_list_view, name="news"),
